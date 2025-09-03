21fa6bfc53ba3ff3dd666abc3ed13fccfca84126
Maria Coelho <132946442+MariaCoelhoM@users.noreply.github.com>
2025-09-03 20:56:35 -0300

    aula 4

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,10 @@
 #include <GL\glew.h>
 #include <GLFW\glfw3.h>
 
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <glm\gtc\type_ptr.hpp>
+
 // VAO é o ponteiro do programa que estou rodando, espaço de memória que representa aquele objeto
 // VBO é o ponteiro para os meus dados brutos, são os dados do objeto a ser representado pelo VAO 
 // shaderProgram é qual programa estou rodando 
@@ -11,19 +15,31 @@
 const GLint WIDTH = 800, HEIGHT = 600; // O GLint vai ser o componente criado direto no glew
 GLuint VAO, VBO, shaderProgram; //um inteiro não sinalizado onde só vai ter valores 0,512 não tem o lado negativo
 
+float toRadians = 3.1415f / 180.0f;
+//float directionY = false;
+bool direction = false, directionSize = false;// quando for falso vamos adicionando se for true vamos subtraindo
+float triOffset = 0.0f, triOffsetMax = 0.7f, triOffsetMin = -0.7f, triIncrement = 0.03f; //por onde vamos caminhar vamos começar no 0 no meio da tela - triOffsetMax maximo que vamos chegar a direita na tela - triOffsetMin valor minimo que chegaremos a esquerda
+//float triOffsetY = 0.0f, triOffsetMaxY = 0.7f, triOffsetMinY = -0.7f, triIncrementY = 0.01f;//qualquer coisa apaga isso
+float triOffsetSize = 0.2f, triOffsetSizeMax = 1.2f, triOffsetSizeMin = 0.2f, triOffsetSizeIncrement = 0.1f;
+float triCurrentAngle = 0.0f, triAngleIncrement = 1.0f;
+
 //código em GLSL - vc2 vai ter o valor de (x e y) 
 // o programa vai pegar a posição de x e y
 // passando um argumento para o inicio do programa (args do C++                        
 // estou passando um argumento de entrada na primeira posiçâo                             
 // esse argumento deve ser um vetor de duas posições
-static const char* vertexShader = "					\n\
-#version 330										\n\
-													\n\
-layout(location=0) in vec2 pos;						\n\
-													\n\
-void main() {										\n\
-	gl_Position = vec4(pos.x, pos.y, 0.0, 1.0);		\n\
-}													\n\
+// layout pode mudar mais depois da execução
+// uniform float posX; - nova posição de dados 
+static const char* vertexShader = "									\n\
+#version 330														\n\
+																	\n\
+layout(location=0) in vec2 pos;										\n\
+uniform mat4 model;													\n\
+																	\n\
+																	\n\
+void main() {														\n\
+	gl_Position = model * vec4(pos.x, pos.y, 0.0, 1.0);				\n\
+}																	\n\
 ";
 
 //fragmento o que podemos mudar do desenho que podemos fazer o vec3 e por causa do RGB
@@ -31,6 +47,7 @@ void main() {										\n\
 // fragment pode ser lido como "a partir desse ponto o que faço com ele?"
 // shader para atribuir cores aos pontos
 // diferente da entrada por layout, uniform é uma entrada em tempo de execução 
+// uniform pode mudar adurante a execução da cor
 static const char* fragmentShader = "				\n\
 #version 330										\n\
 													\n\
@@ -144,10 +161,53 @@ int main() {
 	GLint uniformColor = glGetUniformLocation(shaderProgram, "triColor");
 	glUniform3f(uniformColor, 1.0f, 1.0f, 0.0f); // e pra sair a cor amarela
 
+	//movimenta o triangulo X
+	if (!direction) {
+		triOffset += triIncrement;
+	}
+	else {
+		triOffset -= triIncrement;
+	}
+	if (triOffset > triOffsetMax || triOffset < triOffsetMin) 
+		direction = !direction;
+	triCurrentAngle += triAngleIncrement;
+	
+	if (triCurrentAngle >= 360)
+		triCurrentAngle = 0;
+	if (!directionSize)
+		triOffsetSize += triOffsetSizeIncrement;
+	else
+		triOffsetSize -= triOffsetSizeIncrement;
+	if (triOffsetSize > triOffsetSizeMax || triOffsetSize < triOffsetSizeMin)
+		directionSize = !directionSize;
+
+	GLint uniformModel = glGetUniformLocation(shaderProgram, "model"); // estamos pegando as posições
+	glm::mat4 model(1.0f); //criamos uma matriz 4x4 onde tem 1´s dentro dela
+	model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+	model = glm::scale(model, glm::vec3(triOffsetSize, triOffsetSize, 0.0f));
+	model = glm::rotate(model, triCurrentAngle * toRadians, glm::vec3(1.0f, 0.15f, 0.20f)); // isso aqui faz rotação no x, y,z
+	glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+
+
+	//movimenta o triangulo Y
+	//if (!directionY) {
+	//	triOffsetY += triIncrementY;
+	//}
+	//else {
+	//	triOffsetY -= triIncrementY;
+	//}
+	//if (triOffsetY > triOffsetMaxY || triOffsetY < triOffsetMinY)
+	//	directionY = !directionY;
+
+	//GLint uniformPosY = glGetUniformLocation(shaderProgram, "posY");
+	//glUniform1f(uniformPosY, triOffsetY);
+
+
+
 	// testando com numeros aleatorios 
 
 	GLint R = glGetUniformLocation(shaderProgram, "triColor");
-	glUniform3f(uniformColor, 1.0f, 1.0f, 1.0f);
+	glUniform3f(uniformColor, 1.0f, 1.0f, 0.0f);
 
 	//desenhando o triangulo
 	glUseProgram(shaderProgram);
